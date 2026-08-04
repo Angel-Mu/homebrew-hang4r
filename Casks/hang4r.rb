087e9d1e54d397e890de4f3ca16db9527945eee3
Angel Malavar <angel.malavar@gmail.com>
2026-08-04 12:03:21 -0600

    hang4r 1.0.68

--- Casks/hang4r.rb
+++ Casks/hang4r.rb
@@ -1,6 +1,6 @@
 cask "hang4r" do
-  version "1.0.67"
-  sha256 "ae49424009b3cc0b1eaccb605b2af460d08a34ad0bcbddb564a6b15b8e554d8c"
+  version "1.0.68"
+  sha256 "a6060f74169a7f89e9c4d88dda7b36790e69a5b57e34972bed1fddb03c7d330f"
 
   url "https://github.com/Angel-Mu/hang4r-releases/releases/download/v#{version}/hang4r-#{version}.dmg"
   name "hang4r"
